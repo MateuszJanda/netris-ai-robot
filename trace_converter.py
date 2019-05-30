5b82977da2eeba2ac1d0d2914e33de5939e4c04e
Mateusz Janda <mateusz.janda@gmail.com>
2019-05-30 16:35:39 +0200

    Saving board

--- trace_converter.py
+++ trace_converter.py
@@ -12,6 +12,7 @@ class MoveData:
         self.shape = 0
         self.move = 0
         self.rotate = 0
+        self.board = ''
 
 
 def main():
@@ -20,22 +21,30 @@ def main():
 
     with open('20190529201253.trace', 'r') as f:
         for line in f:
-            packet = line.split():
-            if packet[0] != '[>]':
-                continue
+            packet = line.split()
 
-            if packet[1] == 'NP_newPiece'
-                if m:
-                    moves.append(m)
-                m = MoveData()
-                m.shape = int(packet[2].split('=')[1])
+            if packet[0] == '[>]':
+                if packet[1] == 'NP_newPiece':
+                    if m:
+                        moves.append(m)
+                    m = MoveData()
+                    m.shape = int(packet[2].split('=')[1])
+                elif packet[1] == 'NP_left':
+                    m.move -= 1
+                elif packet[1] == 'NP_right':
+                    m.move += 1
+                elif packet[1] == 'NP_rotate':
+                    m.rotate += 1
+            elif packet[0] == '[<]' and packet[1] == 'NP_boardDump':
+                if len(moves) > 0:
+                    moves[-1].board = packet[3].split('=')[1]
 
-            if packet[1] == 'NP_left':
-                m.move -= 1
-            elif packet[1] == 'NP_right':
-                m.move += 1
-            elif packet[1] == 'NP_rotate':
-                m.rotate += 1
+    draw_board(moves[0])
+
+
+def draw_board(move):
+    for line in [move.board[i:i+4] for i in range(0, len(move.board), 4)]:
+        print(line)
 
 
 if __name__ == '__main__':
