cfdf353ea548cf1310464629e92473c95dc81c3d
Mateusz Janda <mateusz.janda@gmail.com>
2019-05-31 09:57:41 +0200

    Added NP_points, and draw_board() function

--- trace_converter.py
+++ trace_converter.py
@@ -12,39 +12,49 @@ class MoveData:
         self.shape = 0
         self.move = 0
         self.rotate = 0
+        self.points = 0
         self.board = ''
 
 
 def main():
-    moves = []
-    m = None
-
     with open('20190529201253.trace', 'r') as f:
-        for line in f:
-            packet = line.split()
-
-            if packet[0] == '[>]':
-                if packet[1] == 'NP_newPiece':
-                    if m:
-                        moves.append(m)
-                    m = MoveData()
-                    m.shape = int(packet[2].split('=')[1])
-                elif packet[1] == 'NP_left':
-                    m.move -= 1
-                elif packet[1] == 'NP_right':
-                    m.move += 1
-                elif packet[1] == 'NP_rotate':
-                    m.rotate += 1
-            elif packet[0] == '[<]' and packet[1] == 'NP_boardDump':
-                if len(moves) > 0:
-                    moves[-1].board = packet[3].split('=')[1]
+        moves = convert_trace(f)
 
     draw_board(moves[0])
 
 
+def convert_trace(trace_file):
+    moves = []
+    m = None
+    for line in trace_file:
+        packet = line.split()
+
+        if packet[0] == '[>]':
+            if packet[1] == 'NP_newPiece':
+                if m:
+                    moves.append(m)
+                m = MoveData()
+                m.shape = int(packet[2].split('=')[1])
+            elif packet[1] == 'NP_left':
+                m.move -= 1
+            elif packet[1] == 'NP_right':
+                m.move += 1
+            elif packet[1] == 'NP_rotate':
+                m.rotate += 1
+        elif packet[0] == '[<]' and packet[1] == 'NP_points':
+            m.points = int(packet[2].split('=')[1])
+        elif packet[0] == '[<]' and packet[1] == 'NP_boardDump':
+            if len(moves) > 0:
+                board = packet[3].split('=')[1]
+                lines = [board[i:i+4] for i in range(0, len(board), 4)]
+                moves[-1].board = reversed([int(line, 16) for line in lines])
+
+    return moves
+
+
 def draw_board(move):
-    for line in [move.board[i:i+4] for i in range(0, len(move.board), 4)]:
-        print(line)
+    for line in move.board:
+        print('{:010b}'.format(line))
 
 
 if __name__ == '__main__':
