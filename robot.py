ddf53566a9a9683720d855a4d0bcacc5af9b1b2e
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-11 21:59:06 +0200

    Update robot loop

--- robot.py
+++ robot.py
@@ -5,16 +5,30 @@ Author: Mateusz Janda <mateusz janda at gmail com>
 Site: github.com/MateuszJanda
 Ad maiorem Dei gloriam
 """
+import time
+import datetime
 
 
 def main():
-    with open('robot.log', 'w') as f:
+    ts = time.time()
+    log_name = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S.bot')
+
+    with open(log_name, 'w') as f:
         print('Version 1')
         while True:
             cmd = input()
-            f.write('[>]' + cmd + '\n')
+            f.write('[>] ' + cmd + '\n')
+            f.flush()
 
+            if cmd.startswith('NewPiece'):
+                print('Message Lorem ipsum\n')
 
+            if cmd.startswith('Exit'):
+                f.write('[!] Game end\n')
+                f.flush()
+                break
+
+    return 0
 
 
 if __name__ == '__main__':
