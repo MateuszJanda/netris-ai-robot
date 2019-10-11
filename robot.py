19ccc149e45332f10cc9550f77051ef465b7a53a
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-11 23:36:58 +0200

    Sending Right command to server

--- robot.py
+++ robot.py
@@ -11,8 +11,9 @@ import datetime
 
 def main():
     ts = time.time()
-    log_name = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d%H%M%S.bot')
+    log_name = datetime.datetime.fromtimestamp(ts).strftime('robot_%Y%m%d%H%M%S.txt')
 
+    piece_count = None
     with open(log_name, 'w') as f:
         print('Version 1')
         while True:
@@ -21,9 +22,20 @@ def main():
             f.flush()
 
             if cmd.startswith('NewPiece'):
-                print('Message Lorem ipsum\n')
+                piece_count = cmd.split(' ')[1]
+                print('Message Lorem ipsum')
+            elif cmd.startswith('TimeStamp') and piece_count:
+                f.write('[<] Right ' + piece_count + '\n')
+                f.flush()
 
-            if cmd.startswith('Exit'):
+                print('Right ' + piece_count)
+                print('Right ' + piece_count)
+                print('Right ' + piece_count)
+                print('Right ' + piece_count)
+                print('Right ' + piece_count)
+                # print('Message right')
+                piece_count = None
+            elif cmd.startswith('Exit'):
                 f.write('[!] Game end\n')
                 f.flush()
                 break
