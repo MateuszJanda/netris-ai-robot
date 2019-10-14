ae28a89616f8ec402f9e8468d79fa6462936839e
Mateusz Janda <mateusz.janda@gmail.com>
2019-10-14 18:13:50 +0200

    Refactor

--- robot.py
+++ robot.py
@@ -19,66 +19,96 @@ EMPTY_BLOCK = 0
 FULL_BLOCK = 1
 
 
+
+class Robot:
+    def __init__(self, f):
+        self.f = f
+        self.board = [EMPTY_LINE for _ in range(BORAD_HEIGHT)]
+        self.piece_id = None
+
+    def new_pice(self, params):
+        self.piece_id = params[0]
+        return True, ['Message Lorem ipsum']
+
+    def board_size(self, params):
+        scr_id, height, width = [int(p) for p in params]
+
+        if width != BOARD_WIDTH and height != BORAD_HEIGHT:
+            log(self.f, '[!] Validation board size fail %d %d %d %d' % (width, BOARD_WIDTH, height, BORAD_HEIGHT))
+            return False, ['Exit']
+
+        return True, []
+
+    def row_update(self, params):
+        params = [int(p) for p in params]
+
+        if params[0] == SCR_ID:
+            y = params[1]
+            for x, val in enumerate(params[2:]):
+                board[BORAD_HEIGHT - 1 - y][x] = FULL_BLOCK if val != 0 else EMPTY_BLOCK
+
+        return True, []
+
+    def time_stamp(self, params):
+        if not self.piece_id:
+            return True, []
+
+        out = ['Right ' + self.piece_id, 'Right ' + self.piece_id, 'Right ' + self.piece_id]
+        self.piece_id = None
+        return True, out
+
+
+    def exit(self, params):
+        return False, []
+
+
 def main():
     ts = time.time()
     # log_name = datetime.datetime.fromtimestamp(ts).strftime('robot_%Y%m%d%H%M%S.txt')
     log_name = '/dev/pts/3'
 
-    board = [EMPTY_LINE for _ in range(BORAD_HEIGHT)]
-    piece_id = None
-    ver = False
-
     try:
         with open(log_name, 'w') as f:
-            log(f, '[<] Version 1')
-            print('Version 1')
-
-            while True:
-                cmd = input()
-                log(f, '[>] ' + cmd)
-
-                if cmd.startswith('NewPiece'):
-                    piece_id = cmd.split(' ')[1]
-                    log(f, '[<] Message Lorem ipsum')
-                    print('Message Lorem ipsum')
-
-                elif cmd.startswith('BoardSize'):
-                    scr_id, height, width = [int(p) for p in cmd.split(' ')[1:]]
-
-                    if width != BOARD_WIDTH and height != BORAD_HEIGHT:
-                        log(f, '[<] Exit')
-                        log(f, '[!] Validation board size fail %d %d %d %d' % (width, BOARD_WIDTH, height, BORAD_HEIGHT))
-                        print('Exit')
-                        break
-
-                elif cmd.startswith('RowUpdate'):
-                    params = [int(p) for p in cmd.split(' ')[1:]]
-
-                    if params[0] != SCR_ID:
-                        continue
-
-                    y = params[1]
-                    for x, val in enumerate(params[2:]):
-                        board[BORAD_HEIGHT - 1 - y][x] = FULL_BLOCK if val != 0 else EMPTY_BLOCK
-
-                elif cmd.startswith('TimeStamp') and piece_id:
-                    log(f, '[<] Right ' + piece_id)
-                    print('Right ' + piece_id)
-
-                    log(f, '[<] Right ' + piece_id)
-                    print('Right ' + piece_id)
-
-                    log(f, '[<] Right ' + piece_id)
-                    print('Right ' + piece_id)
-                    piece_id = None
-
-                elif cmd.startswith('Exit'):
-                    break
+            fun(f)
     except:
         with open(log_name, 'w') as f:
             traceback.print_exc(file=f)
 
     return 0
+
+
+def fun(f):
+    robot = Robot(f)
+    out_cmd(f, 'Version 1')
+
+    handler = {
+        'NewPiece' : robot.new_pice,
+        'BoardSize' : robot.board_size,
+        'TimeStamp' : robot.time_stamp,
+        'Exit' : robot.exit
+    }
+
+    while True:
+        cmd = input()
+        log(f, '[>] ' + cmd)
+
+        name = cmd.split(' ')[0]
+        if name not in handler:
+            continue
+
+        params = cmd.split(' ')[1:]
+        result, cmds = handler[name](params)
+
+        for c in cmds:
+            out_cmd(f, c)
+
+        if not result:
+            break
+
+
+def out_cmd(file, cmd):
+    log(file, '[<] ' + cmd)
+    print(cmd)
 
 
 def log(file, msg):
