0a1bafa8fbb81c1331a9fca5f6ed8ff51dcd4a5b
Mateusz Janda <mateusz.janda@gmail.com>
2020-02-20 22:35:13 +0100

    Custom model with two separate outputs

--- nn_model.py
+++ nn_model.py
@@ -6,7 +6,6 @@ Site: github.com/MateuszJanda
 Ad maiorem Dei gloriam
 """
 
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 import pickle
 import numpy as np
@@ -20,24 +19,29 @@ ROTATE_SIZE = 4
 
 
 def main():
-    x_train, y_train = load_data()
+    # Input layer
+    inputs = tf.keras.Input(shape=(BOARD_SIZE + PIECE_SIZE,))
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(BOARD_SIZE + PIECE_SIZE)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(SHIFT_SIZE + ROTATE_SIZE)
-    ])
+    # Hidden layers
+    hidden_1 = tf.keras.layers.Dense(128, activation='relu')(inputs)
+    hidden_2 = tf.keras.layers.Dropout(0.2)(hidden_1)
 
+    # Last layer - two separate outputs
+    outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_2)
+    outputs_2 = tf.keras.layers.Dense(ROTATE_SIZE, activation='relu')(hidden_2)
+
+    # All layers together in model
+    model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1, outputs_2])
+
+    # Compile model and loss functions
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
     model.compile(optimizer='adam',
-        loss=loss_fn,
+        loss=[loss_fn, loss_fn],
         metrics=['accuracy'])
 
+    # Read data and fit model
+    x_train, y_train = load_data()
     model.fit(x_train, y_train, epochs=5)
-
-    model.evaluate(x_test,  y_test, verbose=2)
 
 
 def load_data():
