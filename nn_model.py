fb30394f7e74dd05d8c6d909fee547345c77abf2
Mateusz Janda <mateusz.janda@gmail.com>
2020-02-20 22:04:56 +0100

    Buildin model

--- nn_model.py
+++ nn_model.py
@@ -13,8 +13,31 @@ import numpy as np
 import tensorflow as tf
 
 
+BOARD_SIZE = 20*10
+PIECE_SIZE = 1
+SHIFT_SIZE = 10
+ROTATE_SIZE = 4
+
+
 def main():
     x_train, y_train = load_data()
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(BOARD_SIZE + PIECE_SIZE)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(SHIFT_SIZE + ROTATE_SIZE)
+    ])
+
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    model.compile(optimizer='adam',
+        loss=loss_fn,
+        metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+
+    model.evaluate(x_test,  y_test, verbose=2)
 
 
 def load_data():
@@ -26,6 +49,9 @@ def load_data():
     for i, o in data:
         data_input.append(np.array(i))
         data_output.append(np.array(o))
+
+    print("Single input size: %d", len(data_input[0]))
+    print("Single output size: %d", len(data_output[1]))
 
     return np.array(data_input), np.array(data_output)
 
