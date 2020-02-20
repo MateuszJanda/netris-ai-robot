343d8f592a506d472e7188765e9d9b5ce0b58092
Mateusz Janda <mateusz.janda@gmail.com>
2020-02-20 21:30:26 +0100

    Reading data from pickle

--- nn_model.py
+++ nn_model.py
@@ -1,9 +1,34 @@
+#! /usr/bin/env python3
+
+"""
+Author: Mateusz Janda <mateusz janda at gmail com>
+Site: github.com/MateuszJanda
+Ad maiorem Dei gloriam
+"""
+
+
 from __future__ import absolute_import, division, print_function, unicode_literals
+import pickle
+import numpy as np
 import tensorflow as tf
 
 
 def main():
-    pass
+    x_train, y_train = load_data()
+
+
+def load_data():
+    with open("only_wins.pickle", "rb") as f:
+        data = pickle.load(f)
+
+    data_input = []
+    data_output = []
+    for i, o in data:
+        data_input.append(np.array(i))
+        data_output.append(np.array(o))
+
+    return np.array(data_input), np.array(data_output)
+
 
 
 if __name__ == '__main__':
