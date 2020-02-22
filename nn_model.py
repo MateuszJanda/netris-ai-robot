cc4058d397714b63670a81b8aa1452a0c587fa26
Mateusz Janda <mateusz.janda@gmail.com>
2020-02-22 23:21:06 +0100

    Debug version - two softmax outputs

--- nn_model.py
+++ nn_model.py
@@ -31,25 +31,38 @@ def main():
 
     # Last layer - two separate outputs
     # outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_2)
-    outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_1)
+    outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='softmax')(hidden_1)
     # outputs_2 = tf.keras.layers.Dense(ROTATE_SIZE, activation='relu')(hidden_2)
+    outputs_2 = tf.keras.layers.Dense(ROTATE_SIZE, activation='softmax')(hidden_1)
 
     # All layers together in model
-    # model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1, outputs_2])
-    model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1])
+    model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1, outputs_2])
+    # model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1])
 
     # Compile model and loss functions
-    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    # loss_fn_1 = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    # loss_fn_2 = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+    # loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss_fn_1 = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+    loss_fn_2 = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer='adam',
-        loss=[loss_fn],
-        # loss=[loss_fn, loss_fn],
+        # loss=[loss_fn],
+        loss=[loss_fn_1, loss_fn_2],
+        # loss=['categorical_crossentropy', 'categorical_crossentropy'],
         metrics=['accuracy'])
 
     # Read data and fit model
-    x_train, y_train, x_test, y_test = load_data()
-    model.fit(x_train, y_train, epochs=5)
+    x_train, y_shift_train, y_rotate_train, \
+        x_test, y_shift_test, y_rotate_test = load_data()
+    # model.fit(x_train, y_train, epochs=5)
 
-    model.evaluate(x_test, y_test, verbose=2)
+    # model.fit(x=x_train, y={"shift": y_shift_train, "rotate": y_rotate_train},
+    model.fit(x=x_train, y=[y_shift_train, y_rotate_train],
+        epochs=5)
+
+    # model.evaluate(x=x_test, y={"shift": y_shift_test, "rotate": y_rotate_test},
+    model.evaluate(x=x_test, y=[y_shift_test, y_rotate_test],
+        verbose=2)
 
 
 def load_data(split=0.3):
@@ -57,31 +70,41 @@ def load_data(split=0.3):
         data = pickle.load(f)
 
     x_train = []
-    y_train = []
+    y_shift_train = []
+    y_rotate_train = []
     x_test = []
-    y_test = []
-
+    y_shift_test = []
+    y_rotate_test = []
     for i, o in data:
         i[0] = i[0] / PIECE_TYPES
 
-        o = [o[0]]
+        # o = [o[0]]
         if random.random() < split:
             x_test.append(np.array(i))
-            y_test.append(np.array(o))
+            y_shift_test.append(o[0])
+            y_rotate_test.append(o[1])
         else:
             x_train.append(np.array(i))
-            y_train.append(np.array(o))
+            y_shift_train.append(o[0])
+            y_rotate_train.append(o[1])
 
     x_train = np.array(x_train)
-    y_train = np.array(y_train)
+    y_shift_train = np.array(y_shift_train)
+    y_rotate_train = np.array(y_rotate_train)
     x_test = np.array(x_test)
-    y_test = np.array(y_test)
-    print("x_train shape:", x_train.shape)
-    print("y_train shape:", y_train.shape)
-    print("x_test shape:", x_test.shape)
-    print("y_test shape:", y_test.shape)
+    y_shift_test = np.array(y_shift_test)
+    y_rotate_test = np.array(y_rotate_test)
 
-    return x_train, y_train, x_test, y_test
+    print("\nData info")
+    print("x_train shape:", x_train.shape)
+    print("y_shift_train shape:", y_shift_train.shape)
+    print("y_rotate_train shape:", y_rotate_train.shape)
+    print("x_test shape:", x_test.shape)
+    print("y_shift_test shape:", y_shift_test.shape)
+    print("y_rotate_test shape:", y_rotate_test.shape, "\n")
+
+    return x_train, y_shift_train, y_rotate_train, \
+        x_test, y_shift_test, y_rotate_test
 
 
 if __name__ == '__main__':
