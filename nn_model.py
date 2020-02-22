ece4ee37c9b430f80e6bef4ca3b790fe39681222
Mateusz Janda <mateusz.janda@gmail.com>
2020-02-22 22:37:03 +0100

    Debug version for single softmax

--- nn_model.py
+++ nn_model.py
@@ -27,19 +27,22 @@ def main():
 
     # Hidden layers
     hidden_1 = tf.keras.layers.Dense(128, activation='relu')(inputs)
-    hidden_2 = tf.keras.layers.Dropout(0.2)(hidden_1)
+    # hidden_2 = tf.keras.layers.Dropout(0.2)(hidden_1)
 
     # Last layer - two separate outputs
-    outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_2)
-    outputs_2 = tf.keras.layers.Dense(ROTATE_SIZE, activation='relu')(hidden_2)
+    # outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_2)
+    outputs_1 = tf.keras.layers.Dense(SHIFT_SIZE, activation='relu')(hidden_1)
+    # outputs_2 = tf.keras.layers.Dense(ROTATE_SIZE, activation='relu')(hidden_2)
 
     # All layers together in model
-    model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1, outputs_2])
+    # model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1, outputs_2])
+    model = tf.keras.models.Model(inputs=inputs, outputs=[outputs_1])
 
     # Compile model and loss functions
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer='adam',
-        loss=[loss_fn, loss_fn],
+        loss=[loss_fn],
+        # loss=[loss_fn, loss_fn],
         metrics=['accuracy'])
 
     # Read data and fit model
@@ -61,6 +64,7 @@ def load_data(split=0.3):
     for i, o in data:
         i[0] = i[0] / PIECE_TYPES
 
+        o = [o[0]]
         if random.random() < split:
             x_test.append(np.array(i))
             y_test.append(np.array(o))
@@ -68,10 +72,16 @@ def load_data(split=0.3):
             x_train.append(np.array(i))
             y_train.append(np.array(o))
 
-    print("Single input shape: %d" % x_train[0].shape)
-    print("Single output shape: %d" % y_train[1].shape)
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+    print("x_train shape:", x_train.shape)
+    print("y_train shape:", y_train.shape)
+    print("x_test shape:", x_test.shape)
+    print("y_test shape:", y_test.shape)
 
-    return np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
+    return x_train, y_train, x_test, y_test
 
 
 if __name__ == '__main__':
