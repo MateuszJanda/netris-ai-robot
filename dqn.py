0fd163f12499bcddcf073d4ddf3c25f1f732c90c
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-02 20:40:28 +0200

    Added DQN skeleton

---
+++ dqn.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+Author: Mateusz Janda <mateusz janda at gmail com>
+Site: github.com/MateuszJanda
+Ad maiorem Dei gloriam
+"""
+
+
+"""
+Reinforcement learning. Deep Q-Network/Lerning.
+
+References:
+- https://www.quora.com/Artificial-Intelligence-What-is-an-intuitive-explanation-of-how-deep-Q-networks-DQN-work
+
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
