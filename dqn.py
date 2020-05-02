f2586b80ffea103782dded45cd47f6cceb710db6
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-02 21:48:18 +0200

    Update links

--- dqn.py
+++ dqn.py
@@ -11,7 +11,15 @@ Ad maiorem Dei gloriam
 Reinforcement learning. Deep Q-Network/Lerning.
 
 References:
+- "Playing Atari with Deep Reinforcement Learning"by
+  Volodymyr Mnih, Koray Kavukcuoglu, David Silver, Alex Graves,
+  Ioannis Antonoglou, Daan Wierstra, Martin Riedmiller; DeepMind Technologies
+  https://arxiv.org/pdf/1312.5602.pdf
+Daan Wierstra Martin Riedmiller
+DeepMind Technologies
 - https://www.quora.com/Artificial-Intelligence-What-is-an-intuitive-explanation-of-how-deep-Q-networks-DQN-work
+- https://www.youtube.com/watch?v=A5eihauRQvo
+- https://www.youtube.com/watch?v=Hqf__FlRlzg
 
 """
 
