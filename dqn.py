3ed528e86839ef260db6cd37a5530e19b6dd1291
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-10 22:58:02 +0200

    Update links

--- dqn.py
+++ dqn.py
@@ -8,20 +8,21 @@ Ad maiorem Dei gloriam
 
 
 """
-Reinforcement learning. Deep Q-Network/Lerning.
+Reinforcement learning - Deep Q-Network/Learning.
 
-References:
-- "Playing Atari with Deep Reinforcement Learning"by
-  Volodymyr Mnih, Koray Kavukcuoglu, David Silver, Alex Graves,
-  Ioannis Antonoglou, Daan Wierstra, Martin Riedmiller; DeepMind Technologies
-  https://arxiv.org/pdf/1312.5602.pdf
-Daan Wierstra Martin Riedmiller
-DeepMind Technologies
+Useful links:
+- "Playing Atari with Deep Reinforcement Learning" by
+    Volodymyr Mnih, Koray Kavukcuoglu, David Silver, Alex Graves,
+    Ioannis Antonoglou, Daan Wierstra, Martin Riedmiller; DeepMind Technologies
+    https://arxiv.org/pdf/1312.5602.pdf
+- "Training Deep Q Learning and Deep Q Networks (DQN) Intro and Agent -
+    Reinforcement Learning w/ Python Tutorial" by Harrison Kinsley
+    https://pythonprogramming.net/training-deep-q-learning-dqn-reinforcement-learning-python-tutorial/
 - https://www.quora.com/Artificial-Intelligence-What-is-an-intuitive-explanation-of-how-deep-Q-networks-DQN-work
-- https://www.youtube.com/watch?v=A5eihauRQvo
-- https://www.youtube.com/watch?v=Hqf__FlRlzg
-
+- "How to use Q Learning in Video Games Easily" by Siraj Raval
+    https://www.youtube.com/watch?v=A5eihauRQvo
 """
+
 
 def main():
     pass
