e0c90b77baa77a13a4a4fbf2983c87f3d3e3e972
Mateusz Janda <mateusz.janda@gmail.com>
2020-05-13 18:16:06 +0200

    Added raw main

--- dqn.py
+++ dqn.py
@@ -36,6 +36,62 @@ REPLAY_MEMORY_SIZE = 50_000
 MIN_REPLAY_MEMORY_SIZE = 1_000
 
 
+def main():
+    agent = Agent()
+
+    for episodes in range(1, EPISODES + 1):
+        # Restarting episode - reset episode reward and step number
+        episode_reward = 0
+        step = 1
+
+        # Reset environment and get initial state
+        current_state = env.reset()
+
+        # Reset flag and start iterating until episode ends
+        done = False
+        while not done:
+
+            # This part stays mostly the same, the change is to query a model for Q values
+            if np.random.random() > epsilon:
+                # Get action from Q table
+                action = np.argmax(agent.get_qs(current_state))
+            else:
+                # Get random action
+                action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+
+            new_state, reward, done = env.step(action)
+
+            # Transform new continous state to new discrete state and count reward
+            episode_reward += reward
+
+            if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+                env.render()
+
+            # Every step we update replay memory and train main network
+            agent.update_replay_memory((current_state, action, reward, new_state, done))
+            agent.train(done, step)
+
+            current_state = new_state
+            step += 1
+
+        # Append episode reward to a list and log stats (every given number of episodes)
+        ep_rewards.append(episode_reward)
+        if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+            average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+            agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+            # Save model, but only when min reward is greater or equal a set value
+            if min_reward >= MIN_REWARD:
+                agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+        # Decay epsilon
+        if epsilon > MIN_EPSILON:
+            epsilon *= EPSILON_DECAY
+            epsilon = max(MIN_EPSILON, epsilon)
+
+
 class Agent:
     """DQN agent."""
     def __init__(self):
