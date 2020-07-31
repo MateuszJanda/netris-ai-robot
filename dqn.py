8a9b0bbbcf370d82d576b34cd6e69c42d9672a72
Mateusz Janda <mateusz.janda@gmail.com>
2020-07-31 21:03:29 +0200

    Decreased number of nuerons from 3M to 700K

--- dqn.py
+++ dqn.py
@@ -320,10 +320,10 @@ class Agent:
         #
         # filters: (integer) the dimensionality of the output space. Here for
         #   each pixel there will be generated 256 features.
-        model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3),
+        model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3),
             input_shape=(height, width, 1), activation='relu'))
 
-        model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), activation='relu'))
+        model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
 
         model.add(tf.keras.layers.Flatten())
         model.add(tf.keras.layers.Dense(units=64, activation='relu'))
