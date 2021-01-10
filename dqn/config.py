df4b4de86ff13a29831e276b6d25a148e2b362f5
Mateusz Janda <mateusz.janda@gmail.com>
2021-01-10 19:06:50 +0100

    Moved some function from dqn_env to config

--- dqn/config.py
+++ dqn/config.py
@@ -6,6 +6,15 @@ Site: github.com/MateuszJanda/netris-ai-robot
 Ad maiorem Dei gloriam
 """
 
+import tensorflow as tf
+import argparse
+import socket
+import time
+import pickle
+from agent import Agent
+from envirement import Environment
+
+
 HOST = "127.0.0.1"
 PORT = 9800
 
@@ -45,3 +54,122 @@ EPISODES = 20_000                             # Episodes == full games
 # Exploration settings - try/explore random action with probability epsilon
 EPSILON_DECAY = 0.99995                       # Decay epsilon. Smarter NN is, then less random action should be taken
 MIN_EPSILON = 0.02                            # Epsilon shouldn't less than this. We always want to check something new
+
+
+class Transition:
+    def __init__(self, current_state, action, reward, next_state, last_round):
+        self.current_state = current_state
+        self.action = action
+        self.reward = reward
+        self.next_state = next_state
+        self.last_round = last_round
+
+
+class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+    pass
+
+
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description='Netris proxy robot for reinforcement learning (DQN)\n'
+                'Mateusz Janda (c) <mateusz janda at gmail com>\n'
+                'netris-ai-robot project github.com/MateuszJanda/netris-ai-robot\n'
+                '\n'
+                'Robot is waiting for connection from Agent at ' + HOST + ':' + str(PORT) + '\n',
+        usage='Please try to use -h, --help for more informations',
+        epilog=' \n',
+        formatter_class=CustomFormatter)
+
+    parser.add_argument('-e', '--load_episode', required=False, action='store', dest='episode',
+                        help='Load data from idicated episode')
+    parser.add_argument('-g', '--gpu', required=False, action='store_true', dest='gpu',
+                        help='Use GPU (with fixed memory limit to prevent crashes).')
+    parser.add_argument('-p', '--port', required=False, action='store', default=PORT, dest='port',
+                        help='Listen at port')
+
+    args = parser.parse_args()
+    args.port = int(args.port)
+
+    if args.episode:
+        args.episode = int(args.episode)
+
+    return args
+
+
+def set_fixed_memory():
+    """
+    Fixed memory limit to prevent crash.
+    """
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+
+
+def wait_for_connection(sock, port):
+    """
+    Open port for incoming game status.
+    """
+    # Allow reuse port. Useful for testing, when server is killed many times
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, port))
+    sock.listen()
+    print("Starting server at %s:%d" % (HOST, port))
+
+    return sock
+
+
+def create_agent(episode, model):
+    """
+    Create agent from existing snapshot, or create new one.
+    """
+    agent = Agent(model)
+
+    if episode:
+        with open(DATA_SNAPSHOT % episode, "rb") as f:
+            epsilon, agent.replay_memory, _ = pickle.load(f)
+        start_episode = episode + 1
+    else:
+        epsilon = 1
+        start_episode = 0
+
+    return agent, epsilon, start_episode
+
+
+def save_snapshot(agent, epsilon, episode, episode_reward, moves):
+    """Save snapshot."""
+    agent.get_tf_model().save(MODEL_SNAPSHOT % episode)
+
+    with open(DATA_SNAPSHOT % episode, "wb") as f:
+        pickle.dump((epsilon, agent.replay_memory, episode_reward), f)
+
+    with open(STATS_FILE, "a") as f:
+        f.write("Episode: %d, epsilon: %0.2f, moves: %d, reward: %0.2f\n" % (episode, epsilon, moves, episode_reward))
+
+
+def start_learning(sock, episode, play_one_game, model):
+    """
+    Learn through episodes.
+    """
+    env = Environment(sock)
+    model = model(episode)
+    agent, epsilon, start_episode = create_agent(episode, model)
+
+    for episode in range(start_episode, EPISODES + 1):
+        episode_reward, epsilon = play_one_game(epsilon, env, agent)
+
+        if episode > 0 and episode % SNAPSHOT_MODULO == 0:
+            save_snapshot(agent, epsilon, episode, episode_reward, len(env.handling_time))
+
+        print("Episode %d, epsilon %0.3f, reward %0.2f, moves %d, avg handling time: %0.4f, game time: %0.4f"
+            % (episode,
+                epsilon,
+                episode_reward,
+                len(env.handling_time),
+                sum(env.handling_time) / len(env.handling_time),
+                time.time() - env.game_tic))
+
+    env.close()
--- dqn/dqn_env.py
+++ dqn/dqn_env.py
@@ -11,111 +11,17 @@ Ad maiorem Dei gloriam
 Reinforcement learning - Deep Q-Network/Learning.
 """
 
-import tensorflow as tf
 import numpy as np
 import socket
-import time
-import argparse
-import pickle
 from flatnn_model import FlatNnModel
-from envirement import Environment
 from heuristic_solver import HeuristicSolver
-from agent import Agent
 import config
 
 
-def main():
-    args = parse_args()
-
-    # Fixed memory limit to prevent crash
-    if args.gpu:
-        gpus = tf.config.experimental.list_physical_devices('GPU')
-        for gpu in gpus:
-            tf.config.experimental.set_memory_growth(gpu, True)
-        tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        agent, epsilon, start_episode = create_agent(args.episode)
-
-        # Allow reuse port. Useful for testing, when server is killed many times
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((config.HOST, args.port))
-        sock.listen()
-        print("Starting server at %s:%d" % (config.HOST, args.port))
-        env = Environment(sock)
-
-        start_learning(env, epsilon, agent, start_episode)
-
-        env.close()
-
-
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
-    pass
-
-
-def parse_args():
-    """Parse command line arguments."""
-    parser = argparse.ArgumentParser(
-        description='Netris proxy robot for reinforcement learning (DQN)\n'
-                'Mateusz Janda (c) <mateusz janda at gmail com>\n'
-                'netris-ai-robot project github.com/MateuszJanda/netris-ai-robot\n'
-                '\n'
-                'Robot is waiting for connection from Agent at ' + config.HOST + ':' + str(config.PORT) + '\n',
-        usage='Please try to use -h, --help for more informations',
-        epilog=' \n',
-        formatter_class=CustomFormatter)
-
-    parser.add_argument('-e', '--load_episode', required=False, action='store', dest='episode',
-                        help='Load data from idicated episode')
-    parser.add_argument('-g', '--gpu', required=False, action='store_true', dest='gpu',
-                        help='Use GPU (with fixed memory limit to prevent crashes).')
-    parser.add_argument('-p', '--port', required=False, action='store', default=config.PORT, dest='port',
-                        help='Listen at port')
-
-    args = parser.parse_args()
-    args.port = int(args.port)
-
-    if args.episode:
-        args.episode = int(args.episode)
-
-    return args
-
-
-def create_agent(episode):
-    """Create agent from existing snapshot, or create new one."""
-    model = FlatNnModel(episode)
-    agent = Agent(model)
-
-    if episode:
-        with open(config.DATA_SNAPSHOT % episode, "rb") as f:
-            epsilon, agent.replay_memory, _ = pickle.load(f)
-        start_episode = episode + 1
-    else:
-        epsilon = 1
-        start_episode = 0
-
-    return agent, epsilon, start_episode
-
-
-def start_learning(env, epsilon, agent, start_episode):
-    """Learn through episodes."""
-    for episode in range(start_episode, config.EPISODES + 1):
-        episode_reward, epsilon = play_one_game(epsilon, env, agent)
-
-        if episode > 0 and episode % config.SNAPSHOT_MODULO == 0:
-            save_snapshot(agent, epsilon, episode, episode_reward, len(env.handling_time))
-
-        print("Episode %d, epsilon %0.3f, reward %0.2f, moves %d, avg handling time: %0.4f, game time: %0.4f"
-            % (episode,
-                epsilon,
-                episode_reward,
-                len(env.handling_time),
-                sum(env.handling_time) / len(env.handling_time),
-                time.time() - env.game_tic))
-
-
 def play_one_game(epsilon, env, agent):
-    """Play one game."""
+    """
+    Play one game.
+    """
     episode_reward = 0
     solver = HeuristicSolver()
 
@@ -142,7 +48,7 @@ def play_one_game(epsilon, env, agent):
         episode_reward += reward
 
         # Every step update replay memory and train NN model
-        transition = Transition(current_state, action, reward, next_state, last_round)
+        transition = config.Transition(current_state, action, reward, next_state, last_round)
         agent.update_replay_memory(transition)
         agent.train(last_round)
 
@@ -156,32 +62,21 @@ def play_one_game(epsilon, env, agent):
 
 
 def adjust_epsilon(epsilon):
-    """Decay epsilon."""
+    """
+    Decay epsilon.
+    """
     if epsilon > config.MIN_EPSILON:
         epsilon = epsilon * config.EPSILON_DECAY
 
     return epsilon
 
 
-class Transition:
-    def __init__(self, current_state, action, reward, next_state, last_round):
-        self.current_state = current_state
-        self.action = action
-        self.reward = reward
-        self.next_state = next_state
-        self.last_round = last_round
-
-
-def save_snapshot(agent, epsilon, episode, episode_reward, moves):
-    """Save snapshot."""
-    agent.get_tf_model().save(config.MODEL_SNAPSHOT % episode)
-
-    with open(config.DATA_SNAPSHOT % episode, "wb") as f:
-        pickle.dump((epsilon, agent.replay_memory, episode_reward), f)
-
-    with open(config.STATS_FILE, "a") as f:
-        f.write("Episode: %d, epsilon: %0.2f, moves: %d, reward: %0.2f\n" % (episode, epsilon, moves, episode_reward))
-
-
 if __name__ == '__main__':
-    main()
+    args = config.parse_args()
+
+    if args.gpu:
+        config.set_fixed_memory()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        config.wait_for_connection(sock, args.port)
+        config.start_learning(sock, args.episode, play_one_game, model=FlatNnModel)
