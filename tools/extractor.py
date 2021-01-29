584561c148dfe1f25d63cd3c1e69023e5fddd7d7
Mateusz Janda <mateusz.janda@gmail.com>
2021-01-29 21:53:57 +0100

    Refactor

--- tools/extractor.py
+++ tools/extractor.py
@@ -6,18 +6,20 @@ Site: github.com/MateuszJanda/netris-ai-robot
 Ad maiorem Dei gloriam
 """
 
-import os
 import pickle
 import trace_parser as t
 
 
-BOARD_SIZE = 20*10
+BOARD_SIZE = 20 * 10
 PIECE_SIZE = 1
 
 
 def main():
-    only_wins = lambda action: action.points()
-    no_gaps = lambda action: action.points() or (action.gaps() == 0 and not action.cliff(3))
+    def only_wins(action):
+        return action.points()
+
+    def no_gaps(action):
+        return action.points() or (action.gaps() == 0 and not action.cliff(3))
 
     # extract_data(only_wins, "only_wins.pickle")
     extract_data(no_gaps, "no_gaps.pickle")
@@ -49,10 +51,10 @@ def extract_data(action_filter, file_name):
         if total % 100 == 0:
             print("Extracted: %d" % (len(data)))
 
-    print("Total: %d, extracted: %d (%0.2f%%)" % (total, len(data), len(data)/total*100))
+    print("Total: %d, extracted: %d (%0.2f%%)" % (total, len(data), len(data) / total * 100))
     print("Single input size: %d" % len(data[0][0]))
 
-    with open(file_name, "wb") as f
+    with open(file_name, "wb") as f:
         pickle.dump(data, f)
     print("Data saved to: %s" % file_name)
 
