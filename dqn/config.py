6fe92e36074a1db44a4b7052fabd4f6bb70210a5
Mateusz Janda <mateusz.janda@gmail.com>
2021-03-22 20:39:17 +0100

    config cleanup

--- dqn/config.py
+++ dqn/config.py
@@ -13,7 +13,7 @@ import time
 import pickle
 from dqn.envirement import Environment
 
-
+# Server settings
 HOST = "127.0.0.1"
 PORT = 9800
 
@@ -30,28 +30,27 @@ EMPTY_BLOCK = 0
 FULL_BLOCK = 1
 EMPTY_LINE = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-
-SHFIT_OFFSET = 5
-MAX_PIECE_HEIGHT = 4
-MAX_ALLOWED_GAPS = 5
-MAX_CLEARED_LINES = 4
-REWAD_THRESHOLDS = MAX_CLEARED_LINES + (MAX_CLEARED_LINES - 1)
-
 # Snapshot settings
-SNAPSHOT_MODULO = 100
+SNAPSHOT_MODULO = 50
 MODEL_SNAPSHOT = "%05d_model.h5"
 DATA_SNAPSHOT = "%05d_data.pickle"
 
 # Stats settings
-STATS_MODULO = 5
 STATS_FILE = "stats.txt"
 
 # DQN parameters
 DISCOUNT = 0.95                               # Gamma (𝛾) parameter from Bellman equation
 MINIBATCH_SIZE = 128                          # How many steps (samples) to use for training
-REPLAY_MEMORY_SIZE = 40_000                   # Last steps kept for model training
+REPLAY_MEMORY_SIZE = 20_000                   # Last steps kept for model training
 MIN_REPLAY_MEMORY_SIZE = 10 * MINIBATCH_SIZE  # Minimum number of steps in a memory to start training
-EPISODES = 2_000                              # Episodes == full games
+EPISODES = 50_000                             # Episodes == full games
+
+
+# Custom reward settings
+MAX_PIECE_HEIGHT = 4
+MAX_ALLOWED_GAPS = 5
+MAX_CLEARED_LINES = 4
+REWAD_THRESHOLDS = MAX_CLEARED_LINES + (MAX_CLEARED_LINES - 1)
 
 
 class Transition:
@@ -152,7 +151,6 @@ def save_stats(episode, total_rounds, epsilon, episode_reward, episode_lines, mo
     """
     Log and save statistics.
     """
-    # if episode > 0 and episode % STATS_MODULO == 0:
     with open(STATS_FILE, "a") as f:
         f.write("Episode: %d, rounds: %d, epsilon: %0.2f, reward: %0.2f, lines: %d, moves: %d\n"
             % (episode, total_rounds, epsilon, episode_reward, episode_lines, moves))
