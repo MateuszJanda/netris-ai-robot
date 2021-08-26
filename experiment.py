cef6bf6f75414e996ea5b6dfcc1a2e76e26559e4
Mateusz Janda <mateusz.janda@gmail.com>
2021-08-26 20:20:43 +0200

    Rename dqn folder to robot

--- experiment.py
+++ experiment.py
@@ -7,18 +7,18 @@ Ad maiorem Dei gloriam
 """
 
 import socket
-from dqn.flatnn_model import FlatNnModel
-from dqn.cnn_model import CnnModel
-from dqn.sp_model import SpModel
-from dqn.agent import Agent
-from dqn.caching_agent import CachingAgent
-from dqn import simple_dqn
-from dqn import simple_with_solver_dqn
-from dqn import inter_scoring_dqn
-from dqn import inter_scoring_cache_dqn
-from dqn import simple_episode_espsilon
-from dqn import sp_dqn
-from dqn import config
+from robot.flatnn_model import FlatNnModel
+from robot.cnn_model import CnnModel
+from robot.sp_model import SpModel
+from robot.agent import Agent
+from robot.caching_agent import CachingAgent
+from robot import simple_robot
+from robot import simple_with_solver_robot
+from robot import inter_scoring_robot
+from robot import inter_scoring_cache_robot
+from robot import simple_episode_espsilon
+from robot import sp_robot
+from robot import config
 
 
 if __name__ == '__main__':
@@ -35,43 +35,43 @@ if __name__ == '__main__':
 
             model = FlatNnModel(args.episode)
             agent = Agent(model)
-            play_one_game = simple_dqn.play_one_game
+            play_one_game = simple_robot.play_one_game
         elif args.experiment == 2:
             config.log_in_stats("Experiment: %d. Convolutional NN." % args.experiment)
 
             model = CnnModel(args.episode)
             agent = Agent(model)
-            play_one_game = simple_dqn.play_one_game
+            play_one_game = simple_robot.play_one_game
         elif args.experiment == 3:
-            assert sp_dqn.UPDATE_MODEL_ROUND % config.SNAPSHOT_MODULO == 0, \
+            assert sp_robot.UPDATE_MODEL_ROUND % config.SNAPSHOT_MODULO == 0, \
                 "Caching and training model can't differ when snapshot is saved"
-            config.log_in_stats("Experiment: %d. Stevens and Pradhan DQN" % args.experiment)
+            config.log_in_stats("Experiment: %d. Stevens and Pradhan robot" % args.experiment)
 
             training_model = SpModel(args.episode)
             caching_model = SpModel(args.episode)
             agent = CachingAgent(training_model, caching_model)
-            play_one_game = sp_dqn.play_one_game
+            play_one_game = sp_robot.play_one_game
         elif args.experiment == 4:
             config.log_in_stats("Experiment: %d. Flat NN and scoring based on mistakes." % args.experiment)
 
             model = FlatNnModel(args.episode)
             agent = Agent(model)
-            play_one_game = inter_scoring_dqn.play_one_game
+            play_one_game = inter_scoring_robot.play_one_game
         elif args.experiment == 5:
-            assert inter_scoring_cache_dqn.UPDATE_MODEL_ROUND % config.SNAPSHOT_MODULO == 0, \
+            assert inter_scoring_cache_robot.UPDATE_MODEL_ROUND % config.SNAPSHOT_MODULO == 0, \
                 "Caching and training model can't differ when snapshot is saved"
             config.log_in_stats("Experiment: %d. Flat NN with caching and with scoring based on mistakes." % args.experiment)
 
             training_model = FlatNnModel(args.episode)
             caching_model = FlatNnModel(args.episode)
             agent = CachingAgent(training_model, caching_model)
-            play_one_game = inter_scoring_cache_dqn.play_one_game
+            play_one_game = inter_scoring_cache_robot.play_one_game
         elif args.experiment == 6:
             config.log_in_stats("Experiment: %d. Flat NN with solver" % args.experiment)
 
             model = FlatNnModel(args.episode)
             agent = Agent(model)
-            play_one_game = simple_with_solver_dqn.play_one_game
+            play_one_game = simple_with_solver_robot.play_one_game
         elif args.experiment == 7:
             config.log_in_stats("Experiment: %d. Flat NN with epsilon calculated after episode" % args.experiment)
 
