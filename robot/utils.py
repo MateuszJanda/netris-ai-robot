4158f8890d3815153663a121cfb08a81864f37f1
Mateusz Janda <mateusz.janda@gmail.com>
2022-05-03 20:40:36 +0200

    Increased memory limit

--- robot/utils.py
+++ robot/utils.py
@@ -30,7 +30,7 @@ def set_fixed_memory():
 
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
-    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4*1024)])
 
 
 def load_snapshot_metadata(episode, agent):
